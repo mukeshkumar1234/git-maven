96c3b7c80448289202c402896e7b24d659cb014f
mukeshkumar1234 <mk4197762@gmail.com>
2019-04-16 09:05:26 +0530

    modify GgitInEclipse

--- src/com/atozexample/GitInEclipse.java
+++ src/com/atozexample/GitInEclipse.java
@@ -9,6 +9,7 @@ public class GitInEclipse {
 	//this is the main method
 	public static void main(String[] args) {
 		System.out.println("this is the first commite git in eclise");
+		System.out.println("YYY");
 
 	}
 
